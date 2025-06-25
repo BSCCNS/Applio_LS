36263055dbf501ba858d041e80868dbd01fb4507
tomas andrade <tandrade@icc.ub.edu>
2025-06-25 12:34:57 +0200

    update nb

--- extract_feat_script.py
+++ extract_feat_script.py
@@ -2,13 +2,16 @@
 import glob
 from rvc.extract_feat.infer import VoiceConverter
 
-INPUT_WAV_PATH = '/Users/tomasandrade/Documents/BSC/ICHOIR/applio/Applio_LS/assets/audios/Ha/U'
-OUTPUT_FEAT_PATH = "/Users/tomasandrade/Documents/BSC/ICHOIR/applio/Applio_LS/assets/features/Ha_maria/U"
+#INPUT_WAV_PATH = '/Users/tomasandrade/Documents/BSC/ICHOIR/applio/Applio_LS/assets/audios/Ha/U'
+#OUTPUT_FEAT_PATH = "/Users/tomasandrade/Documents/BSC/ICHOIR/applio/Applio_LS/assets/features/Ha_maria/U"
+
+INPUT_WAV_PATH = '/Users/tomasandrade/Documents/BSC/ICHOIR/ls_features/1_16k_wavs_diffsinger'
+OUTPUT_FEAT_PATH = '/Users/tomasandrade/Documents/BSC/ICHOIR/applio/Applio_LS/assets/features/diffsinger_window_filter'
 
 infer_pipeline = VoiceConverter(
                 embedder_model = "contentvec",
-                use_window = False,
-                use_hi_filter = False,
+                use_window = True,
+                use_hi_filter = True,
                 output_feat_path= f'{OUTPUT_FEAT_PATH}'
                 ) 
 
