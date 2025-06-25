7852e5289a3398345aada6b7a99fa4bf86b818c2
tomas andrade <tandrade@icc.ub.edu>
2025-06-25 11:20:10 +0200

    update nb

--- extract_feat_script.py
+++ extract_feat_script.py
@@ -2,8 +2,8 @@
 import glob
 from rvc.extract_feat.infer import VoiceConverter
 
-INPUT_WAV_PATH = '/Users/tomasandrade/Documents/BSC/ICHOIR/applio/Applio_LS/assets/audios/milagro'
-OUTPUT_FEAT_PATH = "/Users/tomasandrade/Documents/BSC/ICHOIR/applio/Applio_LS/assets/features/milagro"
+INPUT_WAV_PATH = '/Users/tomasandrade/Documents/BSC/ICHOIR/applio/Applio_LS/assets/audios/Ha/U'
+OUTPUT_FEAT_PATH = "/Users/tomasandrade/Documents/BSC/ICHOIR/applio/Applio_LS/assets/features/Ha_maria/U"
 
 infer_pipeline = VoiceConverter(
                 embedder_model = "contentvec",
@@ -13,7 +13,6 @@ infer_pipeline = VoiceConverter(
                 ) 
 
 files = glob.glob(f'{INPUT_WAV_PATH}/*.wav')
-print(files)
 
 for input in files:
     infer_pipeline.convert_audio(input)
