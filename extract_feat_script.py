d9e1aca9a04d314f879bb3812c54cd8a8d8e1f2a
tomas andrade <tandrade@icc.ub.edu>
2025-06-26 12:16:22 +0200

    add phones slicing notebook

--- extract_feat_script.py
+++ extract_feat_script.py
@@ -5,13 +5,13 @@ from rvc.extract_feat.infer import VoiceConverter
 #INPUT_WAV_PATH = '/Users/tomasandrade/Documents/BSC/ICHOIR/applio/Applio_LS/assets/audios/Ha/U'
 #OUTPUT_FEAT_PATH = "/Users/tomasandrade/Documents/BSC/ICHOIR/applio/Applio_LS/assets/features/Ha_maria/U"
 
-INPUT_WAV_PATH = '/Users/tomasandrade/Documents/BSC/ICHOIR/ls_features/1_16k_wavs_diffsinger'
-OUTPUT_FEAT_PATH = '/Users/tomasandrade/Documents/BSC/ICHOIR/applio/Applio_LS/assets/features/diffsinger_window_filter'
+INPUT_WAV_PATH = '/Users/tomasandrade/Documents/BSC/ICHOIR/espacio-latente-maria-main/phonems/l/wavs'
+OUTPUT_FEAT_PATH = '/Users/tomasandrade/Documents/BSC/ICHOIR/espacio-latente-maria-main/phonems/l/feat_raw'
 
 infer_pipeline = VoiceConverter(
                 embedder_model = "contentvec",
-                use_window = True,
-                use_hi_filter = True,
+                use_window = False,
+                use_hi_filter = False,
                 output_feat_path= f'{OUTPUT_FEAT_PATH}'
                 ) 
 
