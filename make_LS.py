0f38e0644069ff690061a0f8859fc28df1b8a2a0
tomas andrade <tandrade@icc.ub.edu>
2025-07-27 01:25:58 +0200

    improve make LS

--- make_LS.py
+++ make_LS.py
@@ -1,5 +1,9 @@
 import glob
 import joblib
+import os 
+
+import time
+
 import pandas as pd
 #from pathlib import Path
 import matplotlib.pyplot as plt
@@ -18,38 +22,28 @@ root = '/media/HDD_disk/tomas/ICHOIR/Applio_LS/assets/datasets/GTSinger_ES'
 #feat_paths = glob.glob(f'{root}/feat/layer_2/*.csv')
 text_grid_paths = glob.glob(f'{root}/TextGrid/*.TextGrid')
 
-# df_anotated_all = u.make_anotated_feat_df(feat_paths, 
-#                                           text_grid_paths, 
-#                                           tp_algn = 'text_grid',
-#                                           dataset = 'gt')
-
-# df_info = df_anotated_all['song'].str.split('-', expand=True)
-# df_info.columns = ['speaker', 'book', 'line']
-# df_anotated_all = pd.concat([df_anotated_all, df_info], axis = 1)
-
-# print(df_anotated_all)
-
 silhouette_dict = {}
 
-for layer in [2,3]:
+folder_plots = 'GT_SingerES_output/plots'
+folder_feat_annotated = 'GT_SingerES_output/feat_2d'
 
+os.makedirs('GT_SingerES_output', exist_ok=True)
+
+os.makedirs(folder_plots, exist_ok=True)
+os.makedirs(folder_feat_annotated, exist_ok=True)
+
+T0 = time.time()
+
+for layer in range(1,13):
+
+    t0 = time.time()
     print(f'-------- Working on layer {layer}')
-    #feat_paths = glob.glob(f'{root}/{layers_dict[layer]}/*.csv')
     feat_paths = glob.glob(f'{root}/feat/layer_{layer}/*.csv')
-    #df_anotated = u.make_anotated_feat_df(feat_paths, lab_paths)
-
     df_anotated = u.make_anotated_feat_df(feat_paths, 
                                           text_grid_paths, 
                                           tp_algn = 'text_grid',
                                           dataset = 'gt')
-    # df_info = df_anotated['song'].str.split('-', expand=True)
-    # df_info.columns = ['speaker', 'book', 'line']
-    # df_anotated = pd.concat([df_anotated, df_info], axis = 1)
-    # c1 = df_anotated_all['phone_base'] != '<AP>'
-    # drop_cols = ['phone_base', 'song', 'speaker', 'book', 'line']
-    # df_no_sil = df_anotated_all[c1].reset_index(drop = True)
-    # X_no_sil = df_no_sil.drop(columns=drop_cols).values
-
+    
     ### umap
     print(f'-------- umap')
     umap2 = u.train_umap(
@@ -66,16 +60,18 @@ for layer in [2,3]:
                                                     save_df = False,
                                                     folder = None)
     
+    df_proj_anotated.to_csv(f'{folder_feat_annotated}/feat_2d_layer_{layer}.csv')
+
+
     ### plots
     print(f'-------- plot')
-    #unique_phones = [k for k in df_anotated['phone_base'].value_counts().keys() if k != '<AP>']
     my_phones = list(df_anotated['phone_base'].value_counts().to_dict().keys())[0:15]
     plots.make_tagged_LS_plot(df_proj_anotated,
             phones = my_phones,
             alpha = 0.25, 
             s = 0.1,
             show_global=True)
-    plt.savefig(f'LS_GTSinger_ES_layer_{layer}')
+    plt.savefig(f'{folder_plots}/LS_GTSinger_ES_layer_{layer}')
     
     ### silhouettes
     print(f'-------- silhouette')
@@ -86,4 +82,16 @@ for layer in [2,3]:
     silhouette_dict[layer] = sil_score
     print(f'--------- sil_score {sil_score}')
 
-    print(f'-------- o -------- \n')
+    t1 = time.time()
+    dt = t1 - t0
+    print(f'------------- Time for layer {layer}: {dt}')
+
+df = pd.DataFrame.from_dict(silhouette_dict, orient='index', columns=['silhouette'])
+df_sil = df.reset_index().rename(columns={'index': 'layer'})
+
+df_sil.to_csv(f'{folder_feat_annotated}/silhouette_layers.csv')
+
+T1 = time.time()    
+DT = T1 - T0
+
+print(f'------------- Total Time: {DT}')
