ef447468e9a9f1bfed731b7ee66377ce648ec850
tomas andrade <tandrade@icc.ub.edu>
2025-12-24 14:27:10 +0100

    add 3d to umap test

--- umap_test.py
+++ umap_test.py
@@ -30,8 +30,9 @@ df_anotated = (
     .reset_index(drop=True)
 )
 
+#######################################################################
 
-print(f'-------- umap')
+print(f'-------- umap 2d')
 umap2 = u.train_umap(
 df_anotated,
 exclude_phones = ['SP'],
@@ -41,23 +42,35 @@ min_dist=0.1,
 n_jobs=1,
 save_model = False,
 folder = None)
-print(f'-------- umap finished')
+print(f'-------- umap 2d finished')
     
 df_proj_anotated = u.make_proj_anotated_feat_df(df_anotated, 
                                                 umap2,
                                                 save_df = False,
                                                 folder = None)
 
-df_proj_anotated.to_csv('df_proj_anotated_test.csv')
+df_proj_anotated.to_csv('df_proj_anotated_test_2d.csv')
 
-print(f'-------- Making plot')
-def make_plot(df_proj_anotated):
-    print(f'-------- plot')
-    my_phones = [k for k in df_proj_anotated['phone_base'].value_counts().keys() if k != 'AP']
-    plots.make_tagged_LS_plot(df_proj_anotated,
-            phones = my_phones,
-            alpha = 0.25, 
-            s = 0.1,
-            show_global=True)
-    plt.savefig(f'LS_layer_12_test')
+#######################################################################
+
+print(f'-------- umap 3d')
+umap3 = u.train_umap(
+df_anotated,
+exclude_phones = ['SP'],
+n_components=3, 
+n_neighbors=100, 
+min_dist=0.1,
+n_jobs=1,
+save_model = False,
+folder = None)
+print(f'-------- umap 3d finished')
+    
+df_proj_anotated_3d = u.make_proj_anotated_feat_df(df_anotated, 
+                                                umap3,
+                                                save_df = False,
+                                                folder = None)
+
+df_proj_anotated_3d.to_csv('df_proj_anotated_test_3d.csv')
+
+
 
